f5699d4aa0faaa59182f1b49e4483b63002d7404
CesarNg <bricecesar.ngoy-wessessi@epfedu.fr>
2018-12-03 17:51:01 -0500

    EventHelper

--- app/src/main/java/com/hfad/eatup/api/EventHelper.java
+++ app/src/main/java/com/hfad/eatup/api/EventHelper.java
@@ -2,6 +2,7 @@ package com.hfad.eatup.api;
 
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.hfad.eatup.Model.Event;
 import com.hfad.eatup.Model.User;
@@ -18,11 +19,12 @@ public class EventHelper {
     }
 
     // --- CREATE ---
-    public static Task<Void> createEvent(String title) {
+    public static Task<DocumentReference> createEvent(String title) {
         Event eventToCreate = new Event(title);
 
     return EventHelper.getEventsCollection()
             .document(title)
-            .set(eventToCreate);
+            .collection(COLLECTION_NAME)
+            .add(eventToCreate);
     }
 }
