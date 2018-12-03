21becb34f86275a0373a642a0258d23c34772895
CesarNg <bricecesar.ngoy-wessessi@epfedu.fr>
2018-12-03 15:17:31 -0500

    MAJ Job

--- app/src/main/java/com/hfad/eatup/ProfileFragment.java
+++ app/src/main/java/com/hfad/eatup/ProfileFragment.java
@@ -148,7 +148,9 @@ public class ProfileFragment extends Fragment {
                 @Override
                 public void onSuccess(DocumentSnapshot documentSnapshot) {
                     User currentUser = documentSnapshot.toObject(User.class);
+                    String job = currentUser.getJob();
                     String username = TextUtils.isEmpty(currentUser.getUsername()) ? getString(R.string.info_no_username_found) : currentUser.getUsername();
+                    editJob.setText(job);
                     editName.setText(username);
 
                 }
