b6c49d4ed4c001925fcc5d8a100c26384aab7492
Youness Dendane <youness.dendane1@uqac.ca>
2018-12-05 14:34:42 -0500

    Configured firebase firestore settings to disable offline persistance

--- app/src/main/java/com/hfad/eatup/LoginActivity.java
+++ app/src/main/java/com/hfad/eatup/LoginActivity.java
@@ -22,6 +22,8 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
 import com.google.firebase.firestore.GeoPoint;
 import com.hfad.eatup.api.UserHelper;
 
@@ -37,7 +39,11 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                .setPersistenceEnabled(true)
+                .build();
+        db.setFirestoreSettings(settings);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
